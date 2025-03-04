ce43e3b59a5eb9d83e50cac3371fc901023f6003
isiah-a <isiaha478@gmail.com>
2025-03-04 15:59:13 -0500

    created isBalanced while loop

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -4,7 +4,22 @@ import java.util.List;
 public class ParenSymmetry {
 
     public Boolean isBalanced(String s) {
-        return null;
+        int amount = 0;
+        int idx = 0;
+        while (idx < s.length()) {
+            char c = s.charAt(idx);
+            if (c == '(') {
+                amount++;
+            }
+            if (c == ')') {
+                amount--;
+            }
+            if (idx < 0) {
+                return false;
+            }
+            idx++;
+            }
+        return amount == 0;
     }
 
     private void checkFile(String filename) {
